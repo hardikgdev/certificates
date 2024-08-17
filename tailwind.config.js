04d83e89f667e2ee25eb13ab32d37c75dad331b4
CodingWithHardik <hardikgupta0002@gmail.com>
2024-08-17 23:33:58 +0530

    Nextjs Project setup with color shades

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const { nextui } = require('@nextui-org/react');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [nextui()],
+  darkMode: 'selector',
+  theme: {
+    colors: {
+      primary: '#5c6ac4',
+      secondary: '#ff7849',
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+  }
+};
