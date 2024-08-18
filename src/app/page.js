83c1f27dd77a6f4d5666da474fdfec23634e69d8
CodingWithHardik <hardikgupta0002@gmail.com>
2024-08-18 23:06:03 +0530

    Install shadcn ui and build a navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+
+import * as React from "react"
+import Image from "next/image"
+import Link from "next/link"
+import NavBar from "@/components/blocks/NavBar"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
+      <NavBar />
       <h1>Hello World</h1>
-    </main>
-  );
+    </>
+  )
 }
