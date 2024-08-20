a2dc699209e8f894c266660d291606761349905a
CodingWithHardik <hardikgupta0002@gmail.com>
2024-08-20 11:53:38 +0530

    footer

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import Image from "next/image"
 import Link from "next/link"
 import NavBar from "@/components/blocks/NavBar"
 import Hero from "@/components/blocks/Hero"
+import Footer from "@/components/blocks/Footer"
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Hero />
+      <Footer />
     </>
   )
 }
